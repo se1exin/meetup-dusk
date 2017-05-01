548c8b9316f3fe39242ad814510c31668889c62e
selexin <jacobhmorris@gmail.com>
2017-05-01 21:13:46 +1000

    Fix webpack buildfile

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,6 @@
 const { mix } = require('laravel-mix');
 
 
-mix.js('resources/assets/js/app.js', 'public/js')
-    .combine(['public/js/main.js'], 'public/js/main.js')
+mix.js('resources/assets/js/main.js', 'public/js')
     .less('resources/assets/less/common.less', 'public/css')
     .less('resources/assets/less/simple.less', 'public/css');
